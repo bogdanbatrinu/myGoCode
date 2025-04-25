6b70ce03f66f6b335f2a8c7491bc5afc8f008583
Student <student@mx23-hp>
2025-04-25 14:42:11 +0100

    update

--- task/main.go
+++ task/main.go
@@ -1,17 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	var favBook string
 	var favBookAuthor string
 	var favBookPublicationYear int
+	memory := bufio.NewReader(os.Stdin)
 
 	fmt.Println("What is your favourite book?: ")
+	favBook, _ = memory.ReadString('\n')
 
 	fmt.Println("Who is the author of your favourite book?: ")
-
+	favBookAuthor, _ = memory.ReadString('\n')
 	fmt.Println("What is the publication year of your favourite book?: ")
+	fmt.Scanln(&favBookPublicationYear)
 
 	fmt.Printf("favBook has value: %v and data type %T\n", favBook, favBook)
 	fmt.Printf("favBookAuthor has value: %v and data type %T\n", favBookAuthor, favBookAuthor)
